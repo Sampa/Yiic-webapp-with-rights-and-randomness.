db400d67fbe4fb1a971d298c4a8bd6e0da5c0780
robregonm <robregonm@76fefd42-25d0-11df-92d3-6929c2627b0d>
2010-08-27 16:47:53 +0000

    Yum should show Profiles Fields and Profile Fields Groups statistics only when Profiles are enabled.

--- user/views/statistics/statistics.php
+++ user/views/statistics/statistics.php
@@ -2,20 +2,20 @@
 $this->title = Yii::t('UserModule.user', 'Statistics');
 
 $this->breadcrumbs = array(
-	Yii::t("UserModule.user", 'Users') => array('index'),
-	Yii::t("UserModule.user", 'Statistics'));
+    Yii::t("UserModule.user", 'Users') => array('index'),
+    Yii::t("UserModule.user", 'Statistics'));
 
-$path = Yii::app()->getBasePath(). '/modules/user/views/statistics/statistics.css';
+$path = Yii::app()->getBasePath() . '/modules/user/views/statistics/statistics.css';
 $cssfile = Yii::app()->assetManager->publish($path);
 Yii::app()->clientScript->registerCssFile($cssfile);
 $this->renderPartial('/messages/new_messages');
 
-if(Yii::app()->getModule('user')->debug===true) {
-	echo CHtml::openTag('div', array('style'=>'background-color: red;color:white;'));
-	echo Yii::t('UserModule.user',
-			'You are running the Yii User Management Module {version} in Debug Mode!',
-			array( '{version}'=>Yii::app()->controller->module->version));
-	echo CHtml::closeTag('div');
+if (Yii::app()->getModule('user')->debug === true) {
+    echo CHtml::openTag('div', array('style' => 'background-color: red;color:white;'));
+    echo Yii::t('UserModule.user',
+            'You are running the Yii User Management Module {version} in Debug Mode!',
+            array('{version}' => Yii::app()->controller->module->version));
+    echo CHtml::closeTag('div');
 }
 
 echo '<table style="width:300px;">';
@@ -24,11 +24,14 @@ printf($f, Yum::t('Active users'), $active_users);
 printf($f, Yum::t('Inactive users'), $inactive_users);
 printf($f, Yum::t('Banned users'), $banned_users);
 printf($f, Yum::t('Admin users'), $admin_users);
-if(Yii::app()->controller->module->enableRoles)printf($f, Yum::t('Roles'), $roles);
-if(Yii::app()->controller->module->enableProfiles)printf($f, Yum::t('Profiles'), $profiles);
-printf($f, Yum::t('Profile fields'), $profile_fields);
-printf($f, Yum::t('Profile field groups'), $profile_field_groups);
-printf($f, Yum::t('Messages'), $messages); 
-printf($f, Yum::t('Different users logged in today'), $logins_today); 
+if (Yii::app()->controller->module->enableRoles)
+    printf($f, Yum::t('Roles'), $roles);
+if (Yii::app()->controller->module->enableProfiles) {
+    printf($f, Yum::t('Profiles'), $profiles);
+    printf($f, Yum::t('Profile fields'), $profile_fields);
+    printf($f, Yum::t('Profile field groups'), $profile_field_groups);
+}
+printf($f, Yum::t('Messages'), $messages);
+printf($f, Yum::t('Different users logged in today'), $logins_today);
 echo '</table>';
 ?>
