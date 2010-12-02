b2de01c7d183d0e3a1fb6b95109a575f4a136900
ruizkor <ruizkor@76fefd42-25d0-11df-92d3-6929c2627b0d>
2010-12-02 09:46:38 +0000

    Init Chinese Language

---
+++ user/messages/zh_cn/YumPasswordValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Chinese Language for Yii User Management Module
+ * @author Ray Kwok (ruizkor@gmail.com)
+ */
+return array(
+    'hello' => 'hola',
+    '{attribute} must include at least {num} lower case letters.' => '{attribute} must include at least {num} lower case letters.',
+    '{attribute} must include at least {num} upper case letters.' => '{attribute} must include at least {num} upper case letters.',
+    '{attribute} must include at least {num} digits.' => '{attribute} must include at least {num} digits.',
+    '{attribute} must include at least {num} symbols.' => '{attribute} must include at least {num} symbols.',
+    '{attribute} is too short (min. {num} characters).' => '{attribute} is too short (min. {num} characters).',
+    '{attribute} is too long (max. {num} characters).' => '{attribute} is too long (max. {num} characters).',
+    '{attribute} must not contain whitespace.' => '{attribute} must not contain whitespace.',
+    '{attribute} must not contain more than {num} sequentially repeated characters.' => '{attribute} must not contain more than {num} sequentially repeated characters.',
+);
