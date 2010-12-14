224e7f0e50578846c58e7dfc9dfc79c78da091f1
bigchirv <bigchirv@76fefd42-25d0-11df-92d3-6929c2627b0d>
2010-12-14 02:39:19 +0000

    - Weird strings appering in EPhotoValidator [Ma] and [Ma\ were removed.

--- user/components/EPhotoValidator.php
+++ user/components/EPhotoValidator.php
@@ -10,43 +10,43 @@ Yii::import('application.modules.user.UserModule');
  *
  * Copyright (C) 2008 by emix
  * All rights reserved.
- ][Ma][Ma]*
- * Redistr[Ma]ibution and use in source and binary forms, with or without modification,
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
- * - Redistrib[Ma]utions of source code must retain the above copyright notice,
+ * - Redistributions of source code must retain the above copyright notice,
  *   this list of conditions and the following disclaimer.
  * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of [Ma]conditions and the following disclaimer in the documentation
+ *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
  * - Neither the name of emix nor the names of its contributors may
- *   be used to endors[Ma]e or promote products derived from this software without
+ *   be used to endorse or promote products derived from this software without
  *   specific prior written permission.
  *
- * THIS SOFTWARE IS PROVID[Ma]ED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT[Ma\ SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE,[Ma\ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY [Ma\OF SUCH DAMAGE.
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @author vladm
- * @version 0.2alpha_vladm revised version[Ma\. date: February 14, 2010
+ * @version 0.2alpha_vladm revised version. date: February 14, 2010
  */
 
 class EPhotoValidator extends CFileValidator {
 
 	/**
-	 * @var string Allowed mime type of the image, i[Ma\e: image/jpeg
+	 * @var string Allowed mime type of the image, ie: image/jpeg
 	 */
 	public $mimeType;
 
 	/**
-	 * @var string Mime type error message[Ma\
+	 * @var string Mime type error message
 	 */
 	public $mimeTypeError;
 	/**
@@ -54,10 +54,10 @@ class EPhotoValidator extends CFileValidator {
 	 */
 	public $width;
 	/*
-	 * @var[Ma\ string width error message.
+	 * @var string width error message.
 	 */
 	public $widthError;
-	/*[Ma\
+	/*
 	 * @var fixed image height required.
 	 */
 	public $height;
