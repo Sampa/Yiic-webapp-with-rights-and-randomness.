3f65569abef5cee3a289799f90a49cd0f14ed727
thyseus <thyseus@76fefd42-25d0-11df-92d3-6929c2627b0d>
2011-07-19 06:35:32 +0000

    try to make YumRegistrationForm more flexible

--- registration/models/YumRegistrationForm.php
+++ registration/models/YumRegistrationForm.php
@@ -7,6 +7,8 @@
  * @package Yum.models
  */
 class YumRegistrationForm extends YumUser {
+	public $email;
+	public $newsletter;
 	public $username;
 	public $password;
 	public $street;
@@ -18,6 +20,18 @@ class YumRegistrationForm extends YumUser {
 	public function rules() 
 	{
 		$rules = parent::rules();
+
+		/* FIXME: As soon as i grasp how i can dynamically add variables to a 
+			 class in PHP, i will enable this code snippet for flexibility:
+
+			 $profile = new YumProfile;
+			 $profile_rules = $profile->rules();
+			 foreach($profile_rules as $rule) 
+			 if(isset($rule[0]) && is_string($rule[0]))
+			 $this->${$rule[0]} = ''; 
+
+			 $rules = array_merge($rules, $profile->rules());	 */
+
 
 		$rules[] = array('username', 'required');
 		$rules[] = array('password, verifyPassword', 'required');
