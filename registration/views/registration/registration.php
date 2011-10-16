fa0e2732b598faeea33d6f49db66300184abbba3
thyseus <thyseus@76fefd42-25d0-11df-92d3-6929c2627b0d>
2011-10-16 06:42:07 +0000

    added email field to registration page

--- registration/views/registration/registration.php
+++ registration/views/registration/registration.php
@@ -6,6 +6,7 @@
 <?php $activeform = $this->beginWidget('CActiveForm', array(
 			'id'=>'registration-form',
 			'enableAjaxValidation'=>true,
+			'enableClientValidation'=>true,
 			'focus'=>array($form,'username'),
 			));
 ?>
@@ -17,6 +18,11 @@
 echo $activeform->labelEx($form,'username');
 echo $activeform->textField($form,'username');
 ?> </div>
+
+<div class="row"> <?php
+echo $activeform->labelEx($profile,'email');
+echo $activeform->textField($profile,'email');
+?> </div>  
 
 <div class="row"> <?php
 echo $activeform->labelEx($profile,'firstname');
@@ -46,7 +52,8 @@ echo $activeform->textField($profile,'lastname');
 		<?php $this->widget('CCaptcha'); ?>
 		<?php echo CHtml::activeTextField($form,'verifyCode'); ?>
 		</div>
-		<p class="hint"><?php echo Yum::t('Please enter the letters as they are shown in the image above.'); ?>
+		<p class="hint">
+		<?php echo Yum::t('Please enter the letters as they are shown in the image above.'); ?>
 		<br/><?php echo Yum::t('Letters are not case-sensitive.'); ?></p>
 	</div>
 	<?php endif; ?>
