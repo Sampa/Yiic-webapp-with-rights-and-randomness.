4f33d8deb7cd8b6c911ae49120dd8a28c80390fc
thyseus <thyseus@76fefd42-25d0-11df-92d3-6929c2627b0d>
2012-02-18 10:09:58 +0000

    small enhancements to login widget

--- user/components/LoginWidget.php
+++ user/components/LoginWidget.php
@@ -3,15 +3,18 @@ Yii::import('zii.widgets.CPortlet');
 
 class LoginWidget extends CPortlet
 {
+	public $view = 'quicklogin';
+	public $title = null;
 	public function init()
 	{
-		$this->title=Yii::t('user', 'Login');
+		if($this->title === NULL)
+			$this->title=Yii::t('user', 'Login');
 		parent::init();
 	}
 
 	protected function renderContent()
 	{
-		$this->render('quicklogin', array('model' => new YumUserLogin()));
+		$this->render($this->view, array('model' => new YumUserLogin()));
 	}
 } 
 ?>
